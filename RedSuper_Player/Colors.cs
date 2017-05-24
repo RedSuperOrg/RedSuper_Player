c7cb18872f5416d4b94c79e6a8061205d90bdd71
RedDread <RedDread@REDDREAD-PC>
2017-05-24 23:03:28 +0100

    SEPARATE FILES, TRY RUNNING A VID

---
+++ RedSuper_Player/Colors.cs
@@ -0,0 +1,336 @@
+﻿using RedSuper_Player.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using TagLib;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.DirectX.AudioVideoPlayback;
+namespace RedSuper_Player
+{
+    public partial class Form1: Form
+    {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////// COLORS RELATED /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///// USES GET/SET METHOD TO GET COLOUR FROM SECOND FORM AND APPLY ON MAIN FORM 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Get value, return it, set it on main form
+        /// </summary>
+        /// 
+        public Image panelTopImage
+        {
+            get { return panelTop.BackgroundImage; }
+            set { panelTop.BackgroundImage = value; }
+        }
+
+        public Color panelTopColor
+        {
+            get { return panelTop.BackColor; }
+            set { panelTop.BackColor = value; }
+        }
+
+        public Color colorsButtonActiveForeColor
+        {
+            get { return bunifuThinButtonColors.ActiveForecolor; }
+            set { bunifuThinButtonColors.ActiveForecolor = value; }
+        }
+
+        public Color colorsButtonIdleFillColor
+        {
+            get { return bunifuThinButtonColors.IdleFillColor; }
+            set { bunifuThinButtonColors.IdleFillColor = value; }
+        }
+
+        public Color colorsButtonActiveFillColor
+        {
+            get { return bunifuThinButtonColors.ActiveFillColor; }
+            set { bunifuThinButtonColors.ActiveFillColor = value; }
+        }
+
+        public Color colorsButtonActiveLineColor
+        {
+            get { return bunifuThinButtonColors.ActiveLineColor; }
+            set { bunifuThinButtonColors.ActiveLineColor = value; }
+        }
+
+        public Color colorsButtonForeColor
+        {
+            get { return bunifuThinButtonColors.ForeColor; }
+            set { bunifuThinButtonColors.ForeColor = value; }
+        }
+
+        public Color colorsButtonIdleForeColor
+        {
+            get { return bunifuThinButtonColors.IdleForecolor; }
+            set { bunifuThinButtonColors.IdleForecolor = value; }
+        }
+
+        public Color colorsButtonIdleLineColor
+        {
+            get { return bunifuThinButtonColors.IdleLineColor; }
+            set { bunifuThinButtonColors.IdleLineColor = value; }
+        }
+
+        //////////// Bunifu Related
+
+        public Color aboutButtonActiveForeColor
+        {
+            get { return bunifuThinButtonAbout.ActiveForecolor; }
+            set { bunifuThinButtonAbout.ActiveForecolor = value; }
+        }
+
+        public Color aboutButtonIdleFillColor
+        {
+            get { return bunifuThinButtonAbout.IdleFillColor; }
+            set { bunifuThinButtonAbout.IdleFillColor = value; }
+        }
+
+        public Color aboutButtonActiveFillColor
+        {
+            get { return bunifuThinButtonAbout.ActiveFillColor; }
+            set { bunifuThinButtonAbout.ActiveFillColor = value; }
+        }
+
+        public Color aboutButtonActiveLineColor
+        {
+            get { return bunifuThinButtonAbout.ActiveLineColor; }
+            set { bunifuThinButtonAbout.ActiveLineColor = value; }
+        }
+
+        public Color aboutButtonForeColor
+        {
+            get { return bunifuThinButtonAbout.ForeColor; }
+            set { bunifuThinButtonAbout.ForeColor = value; }
+        }
+
+        public Color aboutButtonIdleForeColor
+        {
+            get { return bunifuThinButtonAbout.IdleForecolor; }
+            set { bunifuThinButtonAbout.IdleForecolor = value; }
+        }
+
+        public Color aboutButtonIdleLineColor
+        {
+            get { return bunifuThinButtonAbout.IdleLineColor; }
+            set { bunifuThinButtonAbout.IdleLineColor = value; }
+        }
+
+        public Color topSideLabel
+        {
+            get { return bunifuCustomLabelVersionText.ForeColor; }
+            set { bunifuCustomLabelVersionText.ForeColor = value; }
+        }
+
+        public Color topMainLabel
+        {
+            get { return bunifuCustomLabelTopText.ForeColor; }
+            set { bunifuCustomLabelTopText.ForeColor = value; }
+        }
+
+        public Image browseButtonIcon
+        {
+            get { return bunifuFlatButtonBrowse.Iconimage; }
+            set { bunifuFlatButtonBrowse.Iconimage = value; }
+        }
+
+        public Color browseButtonOnHoverTextColor
+        {
+            get { return bunifuFlatButtonBrowse.OnHoverTextColor; }
+            set { bunifuFlatButtonBrowse.OnHoverTextColor = value; }
+        }
+
+        public Image videoButtonIcon
+        {
+            get { return bunifuFlatButtonVideo.Iconimage; }
+            set { bunifuFlatButtonVideo.Iconimage = value; }
+        }
+
+        public Color videoButtonOnHoverTextColor
+        {
+            get { return bunifuFlatButtonVideo.OnHoverTextColor; }
+            set { bunifuFlatButtonVideo.OnHoverTextColor = value; }
+        }
+
+        public Image youtubeButtonIcon
+        {
+            get { return bunifuFlatButtonYoutube.Iconimage; }
+            set { bunifuFlatButtonYoutube.Iconimage = value; }
+        }
+
+        public Color youtubeButtonOnHoverTextColor
+        {
+            get { return bunifuFlatButtonYoutube.OnHoverTextColor; }
+            set { bunifuFlatButtonYoutube.OnHoverTextColor = value; }
+        }
+
+        public Image mySongsButtonIcon
+        {
+            get { return bunifuFlatButtonMySongs.Iconimage; }
+            set { bunifuFlatButtonMySongs.Iconimage = value; }
+        }
+
+        public Color mySongsButtonOnHoverTextColor
+        {
+            get { return bunifuFlatButtonMySongs.OnHoverTextColor; }
+            set { bunifuFlatButtonMySongs.OnHoverTextColor = value; }
+        }
+
+        public Image playlistsButtonIcon
+        {
+            get { return bunifuFlatButtonPlaylists.Iconimage; }
+            set { bunifuFlatButtonPlaylists.Iconimage = value; }
+        }
+
+        public Color playlistsButtonOnHoverTextColor
+        {
+            get { return bunifuFlatButtonPlaylists.OnHoverTextColor; }
+            set { bunifuFlatButtonPlaylists.OnHoverTextColor = value; }
+        }
+
+        public Image menuButtonIcon
+        {
+            get { return bunifuImageButtonSlideMenu.Image; }
+            set { bunifuImageButtonSlideMenu.Image = value; }
+        }
+
+        public Image coverIcon
+        {
+            get { return pictureBoxCoverSlider.Image; }
+            set { pictureBoxCoverSlider.Image = value; }
+        }
+
+        public Color trackBarIndicatorColor
+        {
+            get { return bunifuSliderMain.IndicatorColor; }
+            set { bunifuSliderMain.IndicatorColor = value; }
+        }
+
+        public Image startButtonIcon
+        {
+            get { return bunifuImageButtonStart.Image; }
+            set { bunifuImageButtonStart.Image = value; }
+        }
+
+        public Image playButtonIcon
+        {
+            get { return bunifuImageButtonPlay.Image; }
+            set { bunifuImageButtonPlay.Image = value; }
+        }
+
+        public Image endButtonIcon
+        {
+            get { return bunifuImageButtonEnd.Image; }
+            set { bunifuImageButtonEnd.Image = value; }
+        }
+
+        public Image settingsButtonIcon
+        {
+            get { return bunifuImageButtonSettings.Image; }
+            set { bunifuImageButtonSettings.Image = value; }
+        }
+
+        public Image soundButtonIcon
+        {
+            get { return bunifuImageButtonMute.Image; }
+            set { bunifuImageButtonMute.Image = value; }
+        }
+
+        public Image goButtonIcon
+        {
+            get { return bunifuImageButtonGo.Image; }
+            set { bunifuImageButtonGo.Image = value; }
+        }
+
+        public Color volumeSliderIndicatorColor
+        {
+            get { return bunifuSliderVolume.IndicatorColor; }
+            set { bunifuSliderVolume.IndicatorColor = value; }
+        }
+
+        public Image equalizer
+        {
+            get { return pictureBoxEqualizer.Image; }
+            set { pictureBoxEqualizer.Image = value; }
+        }
+
+        ////////////Video Window
+
+        public Image fullScreenIcon
+        {
+            get { return bunifuImageButtonFullScreenVideo.Image; }
+            set { bunifuImageButtonFullScreenVideo.Image = value; }
+        }
+
+        public Image rewindIcon
+        {
+            get { return bunifuImageButtonRewindVideo.Image; }
+            set { bunifuImageButtonRewindVideo.Image = value; }
+        }
+
+        public Image fastForwardIcon
+        {
+            get { return bunifuImageButtonFastForwardVideo.Image; }
+            set { bunifuImageButtonFastForwardVideo.Image = value; }
+        }
+
+        public Image stopIcon
+        {
+            get { return bunifuImageButtonStopVideo.Image; }
+            set { bunifuImageButtonStopVideo.Image = value; }
+        }
+
+        public Color trackBarVideoIndicatorColor
+        {
+            get { return bunifuSliderMainVideo.IndicatorColor; }
+            set { bunifuSliderMainVideo.IndicatorColor = value; }
+        }
+
+        public Color volumeSliderVideoColor
+        {
+            get { return bunifuSliderVolumeVideo.IndicatorColor; }
+            set { bunifuSliderVolumeVideo.IndicatorColor = value; }
+        }
+
+        public Image startVideoIcon
+        {
+            get { return bunifuImageButtonSkipToStartVideo.Image; }
+            set { bunifuImageButtonSkipToStartVideo.Image = value; }
+        }
+
+        public Image playVideoIcon
+        {
+            get { return bunifuImageButtonPlayVideo.Image; }
+            set { bunifuImageButtonPlayVideo.Image = value; }
+        }
+
+        public Image endVideoIcon
+        {
+            get { return bunifuImageButtonSkipToEndVideo.Image; }
+            set { bunifuImageButtonSkipToEndVideo.Image = value; }
+        }
+
+        public Image soundVideoIcon
+        {
+            get { return bunifuImageButtonSoundVideo.Image; }
+            set { bunifuImageButtonSoundVideo.Image = value; }
+        }
+
+        public Image optionsVideoIcon
+        {
+            get { return bunifuImageButtonOptionsVideo.Image; }
+            set { bunifuImageButtonOptionsVideo.Image = value; }
+        }
+    }
+}
---
+++ RedSuper_Player/VideoControls.cs
@@ -0,0 +1,289 @@
+﻿using RedSuper_Player.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using TagLib;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.DirectX.AudioVideoPlayback;
+
+namespace RedSuper_Player
+{
+    public partial class Form1
+    {
+        // LIST CREATED FOR THE VIDEOS ADDED TO THE VIDEO LISTBOX
+        private IList<string> videoList = new BindingList<string>();
+
+        private Video video;
+
+        private void bunifuImageButtonPlayVideo_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("I REACHED HERE");
+            var temp = listBoxVideos.SelectedItem;
+            if (temp == null) return;
+            string videoPath = listBoxVideos.SelectedItem.ToString();
+            if (videoPath != null && !videoPath.Equals(""))
+            {
+                /*
+                Video video = new Video("C:\\Users\\Public\\Videos\\Sample Videos\\Wildlife.wmv");
+                video.Play();
+                */
+
+                try
+                {
+                    Form form1 = new Form();
+                    form1.Show();
+                    form1.WindowState = FormWindowState.Maximized;
+                    Video movie = new Video(videoPath);
+                    movie.Owner = form1;
+                    movie.Size = new Size(1920, 1080);
+                    movie.Play();
+                } catch(Exception error)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+                video = new Video(videoPath, false);
+                video.Owner = panelVideo;
+                video.Size = new Size(1920, 1080);
+            }
+
+        }
+
+        private void bunifuImageBtgyfghgfhgfhghuttonPlayVideo_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("I REACHED HERE");
+            var temp = listBoxVideos.SelectedItem;
+            if (temp == null) return;
+            string videoPath = listBoxVideos.SelectedItem.ToString();
+            if (videoPath != null && !videoPath.Equals(""))
+            {
+                /*
+                try
+                {
+                    video.Stop();
+                    video.Dispose();
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+                */
+                /*
+                pnlVideo.Size = pnlSize;
+                video.Play();
+                tmrVideo.Enabled = true;
+                btnPlayPause.Text = "Pause";
+                video.Ending += Video_Ending;
+                lblVideo.Text = lstVideos.Text;
+                */
+
+                video = new Video(videoPath, false);
+                video.Owner = panelVideo;
+                //panelVideo.Size = panelSize;
+                video.Play();
+
+            }
+
+        }
+
+        private void panelVideo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        // ON 'VIDEO' CLICK ADJUST PANELS FOR VIDEO CONTROLS
+        private void bunifuFlatButtonVideo_Click(object sender, EventArgs e)
+        {
+            type = false;
+            if (panelVideo.Visible == false)
+            //CHECKS CURRENT TYPE TO AVOID RANDOM ANIMATIONS
+            {
+                panelVideo.Visible = true;
+                webBrowserYoutube.Visible = false;
+                panelYoutube.Visible = true;
+                panelVideoTools.Visible = true;
+                panelVideoTools.BringToFront();
+                listBoxVideos.Visible = true;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////// VIDEO RELATED ////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*        
+        private Video video;
+        private string[] videoPaths;
+        private string folderPath = @"C:\Users\";
+        private int selectedIndex = 0;
+        private Size formSize;
+        private Size pnlSize;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formSize = new Size(this.Width, this.Height);
+            pnlSize = new Size(pnlVideo.Width, pnlVideo.Height);
+
+            videoPaths = Directory.GetFiles(folderPath, "*.wmv");
+
+            if (videoPaths != null)
+            {
+                foreach (string path in videoPaths)
+                {
+                    string vid = path.Replace(folderPath, string.Empty);
+                    vid = vid.Replace(".wmv", string.Empty);
+                    lstVideos.Items.Add(vid);
+                }
+            }
+            lstVideos.SelectedIndex = selectedIndex;
+        }
+
+        private void lstVideos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                video.Stop();
+                video.Dispose();
+            }
+            catch { }
+
+            int index = lstVideos.SelectedIndex;
+            selectedIndex = index;
+            video = new Video(videoPaths[index], false);
+            video.Owner = pnlVideo;
+            pnlVideo.Size = pnlSize;
+            video.Play();
+            tmrVideo.Enabled = true;
+            btnPlayPause.Text = "Pause";
+            video.Ending += Video_Ending;
+            lblVideo.Text = lstVideos.Text;
+        }
+
+        private void Video_Ending(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                System.Threading.Thread.Sleep(2000);
+
+                if (InvokeRequired)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        NextVideo();
+                    }));
+                }
+            });
+        }
+
+        private void NextVideo()
+        {
+            int index = lstVideos.SelectedIndex;
+            index++;
+            if (index > videoPaths.Length - 1)
+                index = 0;
+            selectedIndex = index;
+            lstVideos.SelectedIndex = index;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            NextVideo();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            PreviousVideo();
+        }
+
+        private void PreviousVideo()
+        {
+            int index = lstVideos.SelectedIndex;
+            index--;
+            if (index == -1)
+                index = videoPaths.Length - 1;
+            selectedIndex = index;
+            lstVideos.SelectedIndex = index;
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            if (!video.Playing)
+            {
+                video.Play();
+                tmrVideo.Enabled = true;
+                btnPlayPause.Text = "Pause";
+            }
+            else if (video.Playing)
+            {
+                video.Pause();
+                tmrVideo.Enabled = false;
+                btnPlayPause.Text = "Play";
+            }
+        }
+
+        private void btnFullscreen_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            video.Owner = this;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                //exit full screen when escape is pressed
+                FormBorderStyle = FormBorderStyle.Sizable;
+                WindowState = FormWindowState.Normal;
+                this.Size = formSize;
+                video.Owner = pnlVideo;
+                pnlVideo.Size = pnlSize;
+            }
+        }
+
+        private void trackVolume_Scroll(object sender, EventArgs e)
+        {
+            video.Audio.Volume = trackVolume.Value;
+        }
+
+        private void btnVolume_Click(object sender, EventArgs e)
+        {
+            trackVolume.Visible = !trackVolume.Visible;
+        }
+
+        private void tmrVideo_Tick(object sender, EventArgs e)
+        {
+            int currentTime = Convert.ToInt32(video.CurrentPosition);
+            int maxTime = Convert.ToInt32(video.Duration);
+
+            lblVideoPosition.Text = string.Format("{0:00}:{1:00}:{2:00}", currentTime / 3600, (currentTime / 60) % 60, currentTime % 60)
+                                    + " / " +
+                                    string.Format("{0:00}:{1:00}:{2:00}", maxTime / 3600, (maxTime / 60) % 60, maxTime % 60);
+        }
+            */
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
